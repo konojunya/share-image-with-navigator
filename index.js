f8464fbd14047583d61a0ade8a5e21c18a6b9b69
JJ <junya03dance@gmail.com>
2025-07-29 19:26:05 +0900

    update

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const shareButton = document.getElementById('share');
+const shareButton = document.getElementById("share");
 
-const TARGET_URL = "https://placehold.jp/150x150.jpg";
+const TARGET_URL = "https://share-image-with-navigator.vercel.app/card.png";
 
-shareButton.addEventListener('click', async () => {
-    const blob = await fetch(TARGET_URL).then(res => res.blob());
-    const file = new File([blob], 'remote-file.jpg', { type: 'image/jpeg' });
-    const shareData = {
-        title: 'share remote file',
-        text: 'this is a remote file',
-        files: [file]
-    }
+shareButton.addEventListener("click", async () => {
+  const blob = await fetch(TARGET_URL).then((res) => res.blob());
+  const file = new File([blob], "remote-file.jpg", { type: "image/png" });
+  const shareData = {
+    title: "share remote file",
+    text: "this is a remote file",
+    files: [file],
+  };
 
-    if(!navigator.canShare(shareData)) {
-        alert('Your browser does not support sharing files');
-        return;
-    }
+  if (!navigator.canShare(shareData)) {
+    alert("Your browser does not support sharing files");
+    return;
+  }
 
-    try {
-        await navigator.share(shareData);
-    } catch {
-        alert('Error sharing file');
-    }
-})
+  try {
+    await navigator.share(shareData);
+  } catch {
+    alert("Error sharing file");
+  }
+});
+
