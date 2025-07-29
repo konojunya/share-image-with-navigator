aa6664e08c16d034316dabfacf33f39b8c7fa415
JJ <junya03dance@gmail.com>
2025-07-29 19:32:00 +0900

    update

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const shareButton = document.getElementById("share");
+const shareFileButton = document.getElementById("share-file");
+const shareURLButton = document.getElementById("share-url");
 
 const TARGET_URL = "https://share-image-with-navigator.vercel.app/card.png";
 
-shareButton.addEventListener("click", async () => {
+shareFileButton.addEventListener("click", async () => {
   const blob = await fetch(TARGET_URL).then((res) => res.blob());
   const file = new File([blob], "remote-file.jpg", { type: "image/png" });
   const shareData = {
     title: "share remote file",
     text: "this is a remote file",
     files: [file],
+  };
+
+  if (!navigator.canShare(shareData)) {
+    alert("Your browser does not support sharing files");
+    return;
+  }
+
+  try {
+    await navigator.share(shareData);
+  } catch (err) {
+    console.error(err);
+    if (err.message.includes("canceled")) {
+      alert("canceled");
+      return;
+    }
+    alert("Error sharing file");
+  }
+});
+
+shareURLButton.addEventListener("click", async () => {
+  const shareData = {
+    title: "share remote file",
+    text: "this is a remote file",
+    url: "https://example.com",
   };
 
   if (!navigator.canShare(shareData)) {
